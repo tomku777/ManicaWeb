1d88bb4d245832d39ffb59dd1bc15b7101a72c40
Tomas Kutej <tomas_kutej@hotmail.com>
2014-11-05 23:41:52 +0100

    ManicaWeb2

--- SkodaAutoHTMLgrid/js/skoda-auto.js
+++ SkodaAutoHTMLgrid/js/skoda-auto.js
@@ -3,6 +3,7 @@ var position = new google.maps.LatLng(49.995277777778000, 14.202222222222000);
 var mapSmall = new google.maps.Map(document.getElementById('mapSmall'), {
 	mapTypeId: google.maps.MapTypeId.ROADMAP,
 	center: position,
+	scrollwheel: false,
 	zoom: 12
 });
 new google.maps.Marker({
@@ -14,6 +15,7 @@ new google.maps.Marker({
 var mapLarge = new google.maps.Map(document.getElementById('mapLarge'), {
 	mapTypeId: google.maps.MapTypeId.ROADMAP,
 	center: position,
+	scrollwheel: false,
 	zoom: 12
 });
 new google.maps.Marker({
@@ -21,4 +23,19 @@ new google.maps.Marker({
 	position: position,
 	animation: google.maps.Animation.BOUNCE,
 	map: mapLarge
+});
+
+$('.menu-nav').affix({
+	offset: {
+		top: $('.header').height()
+	}
+});
+
+$('#quick-links').on('hide.bs.collapse', function () {
+	$("#quick-links-angle").removeClass("fa-angle-up");
+	$("#quick-links-angle").addClass("fa-angle-down");
+});
+$('#quick-links').on('show.bs.collapse', function () {
+	$("#quick-links-angle").removeClass("fa-angle-down");
+	$("#quick-links-angle").addClass("fa-angle-up");
 });
